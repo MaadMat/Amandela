753cd783651273768f88edd8601c3b54734564b4
Your Name <metthosmasiea@gmail.com>
2024-10-28 17:21:26 +0200

    Adding New Tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,19 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <link rel="canonical" href="http://localhost:3000" />
+        <script id="clarity-script"  type="text/javascript">{`
+
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "opbfflebom");
+        `}
+</script>
       </head>
       <body className={`${inter.className} antialiased  custom:bg-slate-100`}>
         {children}
+
         <SpeedInsights />
       </body>
     </html>
