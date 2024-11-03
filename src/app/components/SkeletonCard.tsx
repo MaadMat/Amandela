6b7e166bd8689dbed6dc2da0e9a8982063d56db8
Your Name <metthosmasiea@gmail.com>
2024-11-03 04:35:12 +0200

    Updating skeleton size x2

--- src/app/components/SkeletonCard.tsx
+++ src/app/components/SkeletonCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SkeletonCard: React.FC = () => {
     return (
-        <div className="animate-pulse flex flex-col items-center justify-center border p-4 m-2 bg-gray-200 w-[18rem] h-[28rem] relative rounded-[25px] custom:border">
+        <div className="animate-pulse flex flex-col items-center justify-center border p-4 m-2 bg-gray-200 w-[18rem] h-[29.5rem] relative rounded-[25px] custom:border">
             <div className="bg-gray-300 h-28 w-full rounded mb-2"></div>
         </div>
     );
